8d17b7abd1412c53f822dd82457081b2781235b5
Gordon <gordon.tinyefuza@gmail.com>
2021-11-04 09:30:18 +0300

    Add and Delete tests

---
+++ src/_mocks_/addItem.js
@@ -0,0 +1,13 @@
+import localStorageMock from "./localStorage";
+
+function addTask(todos) {
+  const newTask = {
+    description: document.getElementById('input-box').value,
+    completed: false,
+    index: todos.length + 1,
+  };
+  todos.push(newTask);
+  localStorageMock.setItem('todo', todos);
+}
+
+export default addTask;
---
+++ src/_mocks_/deleteItem.js
@@ -0,0 +1,12 @@
+import localStorageMock from "./localStorage";
+
+function deleteTask(todo, todos) {
+  let index = todo.index
+  const currentTasks = todos.filter(todo => todo.index !== index);
+  todos.forEach((object, index) => {
+    object.index = index + 1;
+  });
+  localStorageMock.setItem('todo', currentTasks);
+  return currentTasks
+}
+export default deleteTask
